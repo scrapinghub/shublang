a7ee9aadc753aa52dfc5ae347d920c37d8129733
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2019-11-19 12:44:06 +0800

    add example-5 for double 'first' usage

---
+++ examples/test_example5.py
@@ -0,0 +1,12 @@
+from shublang import evaluate
+from parsel import Selector
+
+def test_example_4():
+    with open('examples/resources/test_example_5.html', 'r') as f:
+        html = f.read()
+
+    data = Selector(html).css('#SalesRank::text').extract()
+
+    expression = r"sub(r',', '') | re_search(r'#(\d+)') | filter(lambda x: x) | first | int | first"
+
+    assert evaluate(expression, data) == 12958
