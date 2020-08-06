344990992db55a8acf094ca8f90ed6d692b9e619
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2020-08-06 12:54:28 +0800

    add tox config to test Python 3.5 to 3.8

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     install_requires=[
         'pipe >= 1.5.0',
